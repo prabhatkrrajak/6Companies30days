b9140fea49f13d9f2a4844310d6643f9fdd5974d
Prabhat Kumar Rajak <54627970+prabhatkrrajak@users.noreply.github.com>
2022-01-05 22:11:09 +0530

    Create q14.java

---
+++ Goldman Sachs/q14.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int length = Integer.MAX_VALUE;
+        int left = 0;
+        int sum = 0;
+        
+        for(int i = 0 ;i<nums.length;i++){
+            sum += nums[i];
+            
+            while(sum >= target){
+                length = Math.min(length,i- left +1 );
+                sum -= nums[left];
+                left++;
+            }
+        }
+        
+        return (length != Integer.MAX_VALUE)? length : 0;
+    }
+}
