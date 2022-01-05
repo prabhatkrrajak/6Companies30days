fb5bc7f73ac0049a74a51dfe55bec89f3775f070
Prabhat Kumar Rajak <54627970+prabhatkrrajak@users.noreply.github.com>
2022-01-05 21:41:40 +0530

    Create q7.java

---
+++ Goldman Sachs/q7.java
@@ -0,0 +1,37 @@
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int N = Integer.parseInt(S[0]);
+            int M = Integer.parseInt(S[1]);
+            int K = Integer.parseInt(S[2]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findPosition(N,M,K));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int findPosition(int N , int M , int K) {
+        // code here
+        if(M <= N-K+1)
+        return M+K-1;
+        
+        M = M - (N - K +1);
+        
+        return (M % N == 0) ? N : (M % N);
+    }
+};
